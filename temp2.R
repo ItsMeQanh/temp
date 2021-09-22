e7fbe55247dcb707afaf2ac3ab61fb85277beafc
ItsMeQanh <91209240+ItsMeQanh@users.noreply.github.com>
2021-09-22 23:31:04 +0700

    hey

--- temp2.R
+++ temp2.R
@@ -1,2 +1,11 @@
 100:130
 
+install.packages(sit)
+echo "# temp" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ItsMeQanh/temp.git
+git push -u origin main
+
